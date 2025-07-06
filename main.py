bc879a28575db202d70856d460c6a1ceab60c907
ianisnt412 <ian_isnt@yahoo.co.uk>
2025-07-06 16:44:47 +0100

    added player and circleshape files

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-import pygame as pyg
+import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from player import Player
 
 def main():
-    pyg.init()
+    pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pyg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0   
+   
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        for event in pyg.event.get():
-            if event.type == pyg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pyg.display.flip()
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        print(dt)
+
+    
 
 if __name__ == "__main__":
     main()
