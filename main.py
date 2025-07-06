20c25da66d9182679911faba485cd9c822554b9f
ianisnt412 <ian_isnt@yahoo.co.uk>
2025-07-06 19:31:33 +0100

    added asteroids, updated main.py to fix timing

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
-
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0   
-   
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    asteroid_group = pygame.sprite.Group()
+
+    Player.containers = (updatable_group, drawable_group)
+    Asteroid.containers = (updatable_group, drawable_group, asteroid_group)
+    AsteroidField.containers = (updatable_group)
+
+    asteroid_field = AsteroidField() 
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -17,10 +29,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable_group.update(dt)
+        for drawable in drawable_group:
+            drawable.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+        
         dt = clock.tick(60) / 1000
         print(dt)
 
